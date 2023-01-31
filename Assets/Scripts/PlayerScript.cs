f3d90c0a830cdefa6c96a901087e3054b392a4cf
Enauradave <davidjohnburns@outlook.com>
2023-01-31 14:37:38 +0000

    Player and Destructible Scipts
    Updated player script to include basic variables and basic behavior functions for healing and damage. Added the destructible script with the same but stripped out controls, input actions and healing function.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    Texture2D texture;
     Controls input;
     InputAction movement;
     InputAction jump;
@@ -12,10 +13,22 @@ public class PlayerScript : MonoBehaviour
 
     Rigidbody2D rb;
 
+    bool isAlive;                           // Is the Player alive?
+    Vector3 position;                       // Position Vector x,y,z: z = Depth?
+    float maxHealth = 100.0f;               // The maximum health the Player can have
+    float health;                           // The current health of the Player                    
+    const float maxStamina = 100.0f;        // The maximum stamina of the Player
+    float stamina;                          // The current stamina of the Player
+
+    // Constructor
     private void Awake()
     {
         input = new Controls();
         rb = GetComponent<Rigidbody2D>();
+        isAlive = true;
+        position = new Vector3(0.0f, 0.0f, 0.0f);
+        health = maxHealth;
+
     }
 
     private void OnEnable()
@@ -25,7 +38,29 @@ public class PlayerScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
 
+    }
+
+    // For applying damage to the player
+    void ApplyDamage(float _value)
+    {
+        health -= _value;
+    }
+
+    // For applying healing to the player
+    void ApplyHealing(float _value)
+    {
+        health += _value;
+    }
+
+    // Destroy the player object
+    void Destroy()
+    {
+        rb = null;
+        if(rb = null)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
