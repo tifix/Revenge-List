fc953fb2d16772eda5e7152cc7d9b8c5706e8fbe
Nero-7 <2002086@abertay.ac.uk>
2023-02-07 15:39:01 +0000

    Order in Layer
    -Fixed order in layer
    -Improved collisions

--- Assets/Scripts/OrderInLayer.cs
+++ Assets/Scripts/OrderInLayer.cs
@@ -7,6 +7,7 @@ public class OrderInLayer : MonoBehaviour
 {
     [SerializeField]
     SpriteRenderer sprite;
+    public int yOffset;
     [Tooltip("Leave ticked for moving entities - uncheck to save set order."), SerializeField] bool isSortingUpdating = true;
 
     void Awake()
@@ -14,15 +15,15 @@ public class OrderInLayer : MonoBehaviour
         if (sprite == null) //Safeguard for automatic assigning with safeguard against no SpriteRenderer
         {
             if (TryGetComponent<SpriteRenderer>(out SpriteRenderer SR)) sprite = SR;
-            else Debug.LogWarning("Sprite Ordering script on object <"+ gameObject.name + "> which has no sprite renderer!");
+            else Debug.LogWarning("Sprite Ordering script on object <" + gameObject.name + "> which has no sprite renderer!");
         }
-
-        sprite.sortingOrder = (int)transform.position.z;
+        //sprite.sortingOrder = ((int)Mathf.Round(sprite.transform.position.z) + yOffset);
+        sprite.sortingOrder = 0;
     }
 
     void Update()
     {
-        sprite.sortingOrder = (int)transform.position.z;
+        //sprite.sortingOrder = ((int)Mathf.Round(sprite.transform.position.z) + yOffset);
         if (!isSortingUpdating) DestroyImmediate(this);              //For static objects, the layer shouldn't update for performance sake - destroy script once redundant
     }
 }
