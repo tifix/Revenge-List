57fe0ec3763458a8d47d7250c969936f26c9d5c5
DavidEnauraBurns <davidjohnburns@outlook.com>
2023-03-13 22:17:21 +0000

    Audio Manager changed to using singleton

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -5,47 +5,45 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
-    public static AudioManager instance;
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
 
-    public AudioMixerGroup mixerGroup;
+    private float lowPitchRange = .95f;
+    private float highPitchRange = 1.05f;
 
-    public Sound[] sounds;
+    public static AudioManager instance = null;
 
-    void Awake()
+    private void Awake()
     {
-        if (instance != null)
+        // If there is not already an instance of AudioManager
+        if (instance == null)
         {
+            // set it to this.
+            instance = this;
+        }
+        // If an instance already exists
+        else if (instance != this)
+        {
+            // Obliterate whatever this object is to enforceth the singleton.
             Destroy(gameObject);
         }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
 
-        foreach (Sound s in sounds)
-        {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.loop = s.loop;
-
-            s.source.outputAudioMixerGroup = mixerGroup;
-        }
+        //Set AudioManager to DontDestroyOnLoad, ensuring that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
     }
 
-    public void Play(string sound)
+    // Plays a clip through the sfxSource
+    public void Play(AudioClip clip)
     {
-        Sound s = Array.Find(sounds, item => item.name == sound);
-        if (s == null)
-        {
-            Debug.LogWarning("Sound: " + name + " not found!");
-            return;
-        }
+        sfxSource.clip = clip;
+        sfxSource.Play();
+    }
 
-        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
-        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
-
-        s.source.Play();
+    // Plays a clip through the musicSource
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
     }
 
 }
