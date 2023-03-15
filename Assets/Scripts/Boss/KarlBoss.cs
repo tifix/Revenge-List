f8099dfc2107f6297b8936b99fa42ee3f8e3d404
MIKOLAJ CZARNECKI <2003225@uad.ac.uk>
2023-03-15 11:20:43 +0000

    more dialogue integrations

--- Assets/Scripts/Boss/KarlBoss.cs
+++ Assets/Scripts/Boss/KarlBoss.cs
@@ -1,30 +1,66 @@
+using DS.Data.Save;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 //using DS.Data.Save;
 
-public class KarlBoss : BossClass
+[System.Serializable]
+public class Phases
 {
+    public List<BossAttacks> attacks = new List<BossAttacks>();
+}
+
+public class KarlBoss : MonoBehaviour
+{
+    public bossHealth health;
+
+    public int currentPhase = 0;
+    int currentAttack = 0;
+    float attackTimer = 0;
+    bool canAttack = true;
+
+    public GameObject steak;
+    public GameObject envelope;
+    public GameObject gnome;
+
     public GameObject fireWall;
 
-    protected override void OnEnable()
+    public float playerRadius;
+    public List<Phases> phases = new List<Phases>();
+
+    [Space(3)]
+    [Tooltip("each phase is to have a different QTE, set them here"), SerializeField] List<QTEObject> phase_QTEs;
+    [Tooltip("each phase is to have a different dialogue, set them here"), SerializeField] List<DSGraphSaveDataSO> phase_dialogues;
+    // [Tooltip("each phase will have different dialogue, set it here"), SerializeField] List<DSGraphSaveDataSO> phase_Dialogues;
+
+    PlayerMovement player;
+    private Vector3 knockbackPosition = Vector3.zero; //to avoid player spam-attacking when behind the firewall, knock the player behind the firewall;
+    [SerializeField]
+    Transform camCenter;
+    public Animator anim;
+    [Tooltip("the projectiles are spawned this many s after the animation starts playing"),Range(0,1),SerializeField] float flipAnimationDelay;
+    [Tooltip("the projectiles are spawned this many s after the animation starts playing"),Range(0,1),SerializeField] float throwAnimationDelay;
+
+    private void OnEnable()
     {
-        base.OnEnable();
+        player = FindObjectOfType<PlayerMovement>();
+        knockbackPosition = new Vector3(camCenter.position.x, player.transform.position.y, camCenter.position.z);
         StartParticles();
         if (anim == null) anim = GetComponentInChildren<Animator>();
         AudioManager.instance.Play("BossTrack");
     }
 
-    protected override void Update()
+    void Update()
     {
         if (canAttack)
         {
+            
             attackTimer += Time.deltaTime;
             if (attackTimer >= phases[currentPhase].attacks[currentAttack].waitTime)
             {
-                if (GameManager.instance.cheat_SkipBossPhase) { goto endOfPhase; }
+                if (GameManager.instance.cheat_SkipBossPhase) { goto endOfPhase;  }
 
+                //GameManager.instance.CallShake(5, 0.5f);
                 //Burst attack
                 if (phases[currentPhase].attacks[currentAttack].style == BossAttacks.ProjectileStyle.BURST)
                 {
@@ -35,6 +71,7 @@ public class KarlBoss : BossClass
                         extraTimer += Time.deltaTime;
                         if (extraTimer >= phases[currentPhase].attacks[currentAttack].delay)
                         {
+                            //Attack(phases[currentPhase].attacks[currentAttack]);
                             StartCoroutine(DelayedAttack(phases[currentPhase].attacks[currentAttack]));
                             extraTimer = 0;
                             count--;
@@ -45,6 +82,7 @@ public class KarlBoss : BossClass
                 //Single attack
                 else if (phases[currentPhase].attacks[currentAttack].style == BossAttacks.ProjectileStyle.SINGLE)
                 {
+                    //Attack(phases[currentPhase].attacks[currentAttack]);
                     StartCoroutine(DelayedAttack(phases[currentPhase].attacks[currentAttack]));
                 }
 
@@ -55,7 +93,7 @@ public class KarlBoss : BossClass
                 endOfPhase:
                 if (currentAttack >= phases[currentPhase].attacks.Count || GameManager.instance.cheat_SkipBossPhase)
                 {
-
+                    
                     //End phase
                     EndPhase();
                     //Wait for shield to break
@@ -69,7 +107,6 @@ public class KarlBoss : BossClass
             }
         }
     }
-
     IEnumerator DelayedAttack(BossAttacks a)
     {
         float animCookTime = 1;
@@ -79,14 +116,14 @@ public class KarlBoss : BossClass
                 {
                     //Show the throwing animation for overhead attacks
                     anim.SetTrigger("attackFlipping");
-                    animCookTime = overHeadAnimationDelay;
+                    animCookTime = flipAnimationDelay;
                     break; 
                 }
             case (BossAttacks.ProjectileType.STRAIGHT):
                 {
                     //Show the throwing animation for overhead attacks
                     anim.SetTrigger("attackThrowing");
-                    animCookTime = straightAnimationDelay;
+                    animCookTime = throwAnimationDelay;
                     break;
                 }
 
@@ -95,24 +132,75 @@ public class KarlBoss : BossClass
         Attack(a);
     }
 
-    public override void EndPhase()
+    void Attack(BossAttacks a)
     {
-        base.EndPhase();
+        if(a.type == BossAttacks.ProjectileType.OVERHEAD)
+        {
+            float xOffset = Random.Range(-playerRadius, playerRadius);
+            float zOffset = Random.Range(-playerRadius, playerRadius);
+
+            GameObject temp = Instantiate<GameObject>(steak, player.transform.position + new Vector3(xOffset, 10, zOffset), Quaternion.identity);
+            temp.GetComponent<BossProjectile>().SetSpeed(a.speed + 1);
+            temp.GetComponent<BossProjectile>().SetDistance(a.timeAlive * 2);
+        }
+
+        else if(a.type == BossAttacks.ProjectileType.STRAIGHT)
+        {
+            float zOffset = Random.Range(-10, 10);
+
+            GameObject temp = Instantiate<GameObject>(envelope, transform.position/* + new Vector3(0, 2, 0)*/, Quaternion.identity);    //previous spawn position spawned them underground and insta-despawned
+            temp.GetComponent<BossProjectile>().SetSpeed(a.speed + 5);
+            temp.GetComponent<BossProjectile>().SetDistance(a.timeAlive);
+            Vector3 dir = player.transform.position - temp.transform.position;
+            dir.y = 0;
+            dir.z += zOffset;
+            dir.x += zOffset;
+            temp.GetComponent<BossProjectile>().SetDirection(dir.normalized);
+        }
+
+        else if (a.type == BossAttacks.ProjectileType.GNOME)
+        {
+            GameObject temp = Instantiate<GameObject>(steak, player.transform.position, Quaternion.identity);
+            temp.GetComponent<BossProjectile>().SetSpeed(a.speed);
+            temp.GetComponent<BossProjectile>().SetDistance(a.timeAlive);
+        }
+    }
+
+    public void EndPhase()
+    {
+        ChangeAttack();
         StopParticles();
         anim.SetBool("Vulnerable", true);
     }
 
-    public override void NextPhase()
+    public void NextPhase()
     {
-        base.NextPhase();
+        StartCoroutine(Woosh(1f)); //Knocks the player back into their position when the phase attacks ended right after going out of QTE 
+        currentAttack = 0;
+        currentPhase++;
         ChangeAttack();
         anim.SetBool("Vulnerable", false);
     }
 
-    public override void RepeatPhase()
+    public void RepeatPhase()
     {
-        base.RepeatPhase();
+        StartCoroutine(Woosh(1f)); //Knocks the player back into their position when the phase attacks ended right after going out of QTE 
+        currentAttack = 0;
         ChangeAttack();
+    }
+
+    public void SetQTEandDialogueForRound(int round) 
+    {
+        //health.QTEtriggerPrompt.GetComponent<Inter_TextTrigger>().preUseDialogue = phase_Dialogues[round];
+
+        
+        //QTE needs to be initialie beforehand i.e. instance doesnt exist
+        QTEManager.instance.SetBeatMap(phase_QTEs[round]);
+    }
+
+    public void ChangeAttack()
+    {
+        canAttack = !canAttack;
     }
 
     public void StopParticles()
@@ -126,17 +214,29 @@ public class KarlBoss : BossClass
     }
 
     #region post QTE knockback
-    public override IEnumerator Woosh(float duration) 
+    public IEnumerator Woosh(float duration) 
     {
-        StartCoroutine(base.Woosh(duration));
+        Vector3 init = player.transform.position;
+        float progress = 0;
+        while (progress < 1) 
+        {
+            player.SetLockMovement();
+            player.GetComponent<Collider>().enabled = false;
+
+            yield return new WaitForFixedUpdate();
+
+            player.transform.position = Vector3.Lerp(init, knockbackPosition, progress);
+            progress += Time.fixedDeltaTime/ duration;
+        }
+        player.SetUnLockMovement();
+        player.GetComponent<Collider>().enabled = true;
         StartParticles();
-        yield return new Null();
     }
     #endregion
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position + new Vector3(0, 2, 0),.25f);
+        Gizmos.DrawSphere(transform.position + new Vector3(0, 2, 0),.25f);
     }
 }
