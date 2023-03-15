3f2b83f0f035f74e862c592a8d320955ae49fc27
Enauradave <1901496@uad.ac.uk>
2023-03-15 15:46:30 +0000

    Added music to Credits scene

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class MenuScript : MonoBehaviour
 {
+
+    public void Start()
+    {
+
+        PlayTrack("CreditsTrack");
+    }
+
     public GameObject CreditsPanel;
 
     public void ToggleCreditsPanel()=>CreditsPanel.SetActive(!CreditsPanel.activeInHierarchy);
     
+    public void PlayTrack(string name)
+    {
+        AudioManager.instance.PlayMusic(name);
+    }
 
 
 }
