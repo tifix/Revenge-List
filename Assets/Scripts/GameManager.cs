fab02a19f199998b6c306fe5a1e6dda34f8f900a
Nero-7 <2002086@abertay.ac.uk>
2023-03-21 13:39:29 +0000

    Scene Transitions
    -Removed "DontDestroyOnLoad"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,7 +38,7 @@ public class GameManager : MonoBehaviour
         if (instance == null) 
         { 
             instance = this; 
-            DontDestroyOnLoad(gameObject); 
+            //DontDestroyOnLoad(gameObject); 
         }
         else Destroy(this);
     }
