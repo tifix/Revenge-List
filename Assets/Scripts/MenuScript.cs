5c0ea90a876d5c9c857507dfe4cd9691dafd3c35
DavidEnauraBurns <59957854+DavidEnauraBurns@users.noreply.github.com>
2023-03-29 14:29:29 +0100

    Added Main Menu Click feedback in Call backs

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -16,8 +16,18 @@ public class MenuScript : MonoBehaviour
         PlayTrack("CreditsTrack");
     }
 
-    public void ToggleCreditsPanel()=>CreditsPanel.SetActive(!CreditsPanel.activeInHierarchy);
-    public void ToggleSettingsPanel()=> SettingsPanel.SetActive(!SettingsPanel.activeInHierarchy);
+    public void ToggleCreditsPanel()
+    {
+        AudioManager.instance.PlaySFX("MenuClick");
+        CreditsPanel.SetActive(!CreditsPanel.activeInHierarchy);
+    }
+
+    public void ToggleSettingsPanel()
+    {
+        AudioManager.instance.PlaySFX("MenuClick");
+        SettingsPanel.SetActive(!SettingsPanel.activeInHierarchy);
+    }
+
 
     public void SetTypingSpeed(float typeRate) => Settings.instance.typingWait = Mathf.Lerp(0.04f, 0.01f, typeRate); //left to slow, right to fast
     public void SetVolume(float value) { float t = Mathf.Lerp(-80, 0, value); Debug.Log(t); AudioMixer.SetFloat("masterVolume", t); Settings.instance.volume = t; }  //left to mute, right to loud
